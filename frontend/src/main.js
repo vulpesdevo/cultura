b6e28b67a6097c98d60d4955d317b3cba10b42e5
VulpesDevo <vulpescodev@gmail.com>
2024-04-30 23:05:08 +0800

    Start

---
+++ frontend/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./routes";
+import axios from "axios";
+
+const app = createApp(App);
+
+axios.defaults.baseURL = "http://127.0.0.1:8000";
+app.use(router, axios);
+
+
+app.mount("#app");
