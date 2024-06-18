c94ce2ca4b9ac8bd054ad082086a016bf996c446
rin0o0 <129216396+rin0o0@users.noreply.github.com>
2024-06-18 19:38:48 +0800

    added notification screen

---
+++ frontend/src/components/Notifications.vue
@@ -0,0 +1,34 @@
+<template>
+    
+    <div class="flex flex-col items-center align-middle w-full px-5 py-5 4 ml-0 overflow-auto h-screen bg-field pt-20 sm:px-28 sm:ml-64 sm:pt-3">
+        <div class="flex  w-full justify-between sm:w-3/4">
+            <h1 class="font-montserrat text-left text-prime font-semibold sm:text-3xl">Notifications</h1>
+            <div class="flex items-center">
+                <h1 class="font-montserrat text-prime text-right sm:text-xl"> Mark all as read | </h1>
+                <span class="text-prime material-icons-outlined sm:pr-2">settings</span>
+            </div>
+        </div> 
+        <div class=" w-full mt-5 justify-between sm:mt-5 sm:w-3/4">
+            <div class="w-full mt-6 pt-4 px-6 h-28 rounded-lg shadow-lg bg-interface sm:mt-5 sm:pt-3 sm:px-6 sm:h-32">
+                <div class="flex justify-between"> 
+                    <div class="flex-col sm:flex ">
+                        <h1 class="font-montserrat text-start text-prime mr-2 text sm:text-xl sm:mr-5">@Barut</h1>
+                        <p class="font-bebas-neue overflow-hidden whitespace-nowrap text-ellipsis text-lg w-[21rem] sm:w-[45rem] text-prime sm:text-2xl">Replied TO YOUR COMMENT IN BEST PULLED NOODLES IN BINONDO asdasdas</p>
+                    </div>
+                    <h1 class="font-montserrat text-end text-prime right-10 fixed sm:text-xl sm:relative "> 5/21/2024</h1>
+                </div>
+                <p class="font-montserrat overflow-hidden whitespace-nowrap text-ellipsis text-sm w-[21rem] sm:w-[63rem] text-prime sm:text-xl text-gray-500"> Slurp-worthy satisfaction awaits at Lan-Zhou Lamien, where every strand of their hand-pulled noodles tells a tale of culinary mastery. Taste the tradition, savor the excellence, and discover why they're Manila's noodle maestros.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
