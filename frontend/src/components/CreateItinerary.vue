6d9b06b1478cee4d523bcf0849094d46b10ee20a
VulpesDevo <vulpescodev@gmail.com>
2024-06-23 13:45:42 +0800

    test-comparing

--- frontend/src/components/CreateItinerary.vue
+++ frontend/src/components/CreateItinerary.vue
@@ -293,7 +293,6 @@ export default {
 					}
 				);
 
-				// Add listener for the place_changed event
 				autocomplete.addListener("place_changed", () => {
 					// Get the place object from the autocomplete widget
 					const place = autocomplete.getPlace();
