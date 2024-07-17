4fbbc909dc82195fc83f9a875a06aca02f287175
VulpesDevo <vulpescodev@gmail.com>
2024-07-18 02:02:00 +0800

    upgraded pip

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,15 +131,15 @@ DATABASES["default"] = dj_database_url.parse(database_url)
 # from pymongo.mongo_client import MongoClient
 # from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://culturalink:2sg5IrIemQjd0d1S@cluster0.ajrds80.mongodb.net/"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi("1"))
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command("ping")
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+# uri = "mongodb+srv://culturalink:2sg5IrIemQjd0d1S@cluster0.ajrds80.mongodb.net/"
+# # Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi("1"))
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command("ping")
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
 # Create a new client and connect to the server
 # client = MongoClient(
 #     "mongodb+srv://culturalink:2sg5IrIemQjd0d1S@cluster0.ajrds80.mongodb.net/"
